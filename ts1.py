403d42816e5c91300332655f699ee57172e0232d
Nicholas-Rotondo <nicholaslouis1994@gmail.com>
2022-10-10 15:41:39 -0400

    updated ts1

---
+++ ts1.py
@@ -0,0 +1,33 @@
+import threading
+import time
+import random
+import socket
+
+dns_table = {
+    'domain_name': 'string',
+    'ip_address': 'string',
+    'rr_type': 'A'
+}
+
+try:
+    ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("[S]: Server socket created")
+except socket.error as err:
+    print('socket open error: {}\n'.format(err))
+    exit()
+
+def get_connection():
+    server_binding = ('', 50007)
+    ss.bind(server_binding)
+    ss.listen(1)
+    host = socket.gethostname()
+    print("[S]: Server host name is {}".format(host))
+    localhost_ip = (socket.gethostbyname(host))
+    print("[S]: Server IP address is {}".format(localhost_ip))
+    csockid, addr = ss.accept()
+    print ("[S]: Got a connection request from a client at {}".format(addr))
+
+
+get_connection()
+ss.close()
+exit()
