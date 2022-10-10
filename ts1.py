89a5c3b2ac8c14dc75b1262b4e4b0da17a0f5cf6
Nicholas-Rotondo <nicholaslouis1994@gmail.com>
2022-10-10 16:17:41 -0400

    ts2 & ts1

--- ts1.py
+++ ts1.py
@@ -5,12 +5,6 @@ import socket
 
 print("hello")
 
-dns_table = {
-    'domain_name': 'string',
-    'ip_address': 'string',
-    'rr_type': 'A'
-}
-
 try:
     ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("[S]: Server socket created")
@@ -19,7 +13,7 @@ except socket.error as err:
     exit()
 
 def get_connection():
-    server_binding = ('', 50007)
+    server_binding = ('', 50008)
     ss.bind(server_binding)
     ss.listen(1)
     host = socket.gethostname()
@@ -28,6 +22,18 @@ def get_connection():
     print("[S]: Server IP address is {}".format(localhost_ip))
     csockid, addr = ss.accept()
     print ("[S]: Got a connection request from a client at {}".format(addr))
+    csockid.send(msg.encode('utf-8'))
+
+    file_name = "./out-proj.txt"
+    # write elements in data into file_name with each element in a seperate line
+    with open(file_name, 'w') as f:
+        flag = True
+        while flag:
+            line = csockid.recv(50007)
+            if(line == ""): 
+                flag = False
+                break
+            f.write(line.decode('utf-8') + '\n')
 
 
 get_connection()
--- ts2.py
+++ ts2.py
@@ -5,12 +5,6 @@ import socket
 
 print("hello")
 
-dns_table = {
-    'domain_name': 'string',
-    'ip_address': 'string',
-    'rr_type': 'A'
-}
-
 try:
     ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("[S]: Server socket created")
@@ -28,6 +22,18 @@ def get_connection():
     print("[S]: Server IP address is {}".format(localhost_ip))
     csockid, addr = ss.accept()
     print ("[S]: Got a connection request from a client at {}".format(addr))
+    csockid.send(msg.encode('utf-8'))
+
+    file_name = "./out-proj.txt"
+    # write elements in data into file_name with each element in a seperate line
+    with open(file_name, 'w') as f:
+        flag = True
+        while flag:
+            line = csockid.recv(50007)
+            if(line == ""): 
+                flag = False
+                break
+            f.write(line.decode('utf-8') + '\n')
 
 
 get_connection()
