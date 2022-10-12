e028687466276c472d1d1c5b3d6b20e326bdf82f
Michael Muzafarov <mmm606@scarletmail.rutgers.edu>
2022-10-12 14:16:58 -0400

    new ls

--- ls.py
+++ ls.py
@@ -53,14 +53,33 @@ class Server:
         self.send_to_client(gotten)
 
 
+    def get_and_send(self):
+        dns_name = self.receive_from_client()
+        while(dns_name):
+            self.dns_request(dns_name)
+            response = self.get_ts_response(dns_name)
+            self.send_to_client(response)
+            dns_name = self.receive_from_client()
+
     def send_to_client(self, msg):
         self.client.send(msg.encode('utf-8'))
 
         
     def receive_from_client(self):
-        raw_dat = self.client.recv(220)
-        cleaned = raw_dat.decode('utf-8')
-        return cleaned
+        #raw_dat = self.client.recv(220)
+        #cleaned = raw_dat.decode('utf-8')
+        #return cleaned
+        can_read, can_write, exceps = select.select([self.client], [], [], 10)
+        data = 'nothing read'
+        if(len(can_read) == 0):
+            time.sleep(3)
+            if(len(can_read) == 0):
+                #nothinghere.com - TIMED OUT
+                return False
+                
+        for i in can_read:
+            data = i.recv(220)
+        return data
 
 
     def dns_request(self, name):
